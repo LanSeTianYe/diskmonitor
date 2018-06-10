6f6780b3a7d0b9fa58c2f42bb30b56dc353bcdaf
sunfeilong <sunfeilong1993@gmail.com>
2018-06-10 13:14:05 +0800

    sql

--- sql/init.sql
+++ sql/init.sql
@@ -1,2 +1,24 @@
 -- 创建数据库
 CREATE DATABASE IF NOT EXISTS disk_monitor default character set utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- 文件基本信息
+DROP TABLE file_base_info;
+
+CREATE TABLE `file_base_info` (
+  `file_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `file_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `file_path` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `file_type` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `is_directory` bit(1) DEFAULT NULL,
+  `record_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`file_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+
+DROP TABLE file_size;
+-- 文件大小表
+CREATE TABLE `file_size` (
+  `record_date` datetime NOT NULL,
+  `file_base_info_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `file_size` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`record_date`,`file_base_info_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
